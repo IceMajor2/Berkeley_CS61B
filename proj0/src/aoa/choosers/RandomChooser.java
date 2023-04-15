3b51843886b1ae0976c2ee5388117972b6f91619
IceMajor <icemajorr2@gmail.com>
2023-04-15 11:38:00 +0200

    Game's playable, RandomChooser functions

--- proj0/src/aoa/choosers/RandomChooser.java
+++ proj0/src/aoa/choosers/RandomChooser.java
@@ -8,25 +8,42 @@ public class RandomChooser implements Chooser {
     private String pattern;
 
     public RandomChooser(int wordLength, String dictionaryFile) {
-        // TODO: Fill in/change this constructor.
-        chosenWord = "";
+        if(wordLength < 1) {
+            throw new IllegalArgumentException();
+        }
+        var words = FileUtils.readWordsOfLength(dictionaryFile, wordLength);
+        if(words.isEmpty()) {
+            throw new IllegalStateException();
+        }
+        this.pattern = "";
+        for(int i = 0; i < wordLength; i++) {
+            pattern += "-";
+        }
+        this.chosenWord = words.get(StdRandom.uniform(words.size()));
     }
 
     @Override
     public int makeGuess(char letter) {
-        // TODO: Fill in this method.
-        return 0;
+        int occurs = 0;
+        for(int i = 0; i < chosenWord.length(); i++) {
+            char ch = chosenWord.charAt(i);
+            if(ch == letter) {
+                occurs++;
+                StringBuilder sb = new StringBuilder(pattern);
+                sb.replace(i, i + 1, Character.toString(letter));
+                pattern = sb.toString();
+            }
+        }
+        return occurs;
     }
 
     @Override
     public String getPattern() {
-        // TODO: Fill in this method.
-        return "";
+        return pattern;
     }
 
     @Override
     public String getWord() {
-        // TODO: Fill in this method.
-        return "";
+        return chosenWord;
     }
 }
