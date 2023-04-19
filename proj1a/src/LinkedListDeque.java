3459eb2e903118e1a19e4b38609c212ad6d3ea07
IceMajor <icemajorr2@gmail.com>
2023-04-19 17:11:15 +0200

    Foundation of addFirst and addLast methods. "toList" method implementation

--- proj1a/src/LinkedListDeque.java
+++ proj1a/src/LinkedListDeque.java
@@ -1,41 +1,79 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class LinkedListDeque<T> implements Deque<T> {
 
     public static void main(String[] args) {
         Deque<Integer> LLD = new LinkedListDeque<>();
+        LLD.addLast(-46);
+        LLD.addFirst(5);
+        LLD.addFirst(-11);
+        LLD.addLast(9);
+        LLD.addFirst(6);
+        System.out.println(LLD.toList());
     }
 
     private Node sentinel;
     private int size;
 
     public LinkedListDeque() {
+        this.sentinel = new Node(null, 2141235, null);
 
+        // SLList below
+//        this.sentinel = new Node(124456, null);
+//        this.sentinel.next = new Node<>(item, null);
+        this.size = 0;
     }
 
     @Override
     public void addFirst(T x) {
-
+        size++;
+        if(size == 1) {
+            addFirstItem(x);
+            return;
+        }
+        sentinel.next = new Node(sentinel, x, sentinel.next);
+        sentinel.next.next.prev = sentinel.next;
     }
 
     @Override
     public void addLast(T x) {
+        size++;
+        if(size == 1) {
+            addFirstItem(x);
+            return;
+        }
+        sentinel.prev = new Node(sentinel.prev, x, sentinel);
+        sentinel.prev.prev.next = sentinel.prev;
+    }
 
+    private void addFirstItem(T x) {
+        sentinel.next = new Node(sentinel, x, sentinel);
+        sentinel.prev = sentinel.next;
     }
 
     @Override
     public List<T> toList() {
-        return null;
+        if(size == 0) {
+            return null;
+        }
+        List<T> asList = new ArrayList<>();
+        Node<T> pointer = sentinel.next;
+        while(pointer != sentinel) {
+            asList.add(pointer.item);
+            pointer = pointer.next;
+        }
+        return asList;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
@@ -58,11 +96,11 @@ public class LinkedListDeque<T> implements Deque<T> {
         return null;
     }
 
-    private class Node {
+    private class Node<T> {
 
-        private Node prev;
+        private Node<T> prev;
         private T item;
-        private Node next;
+        private Node<T> next;
 
         public Node(Node prev, T item, Node next) {
             this.prev = prev;
