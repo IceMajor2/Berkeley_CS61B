5f892c03ac8a493d7a21ca2493da223c56bc9999
IceMajor <icemajorr2@gmail.com>
2023-04-19 12:21:50 +0200

    SLList

--- my_sandbox/DLList/src/main/java/SLList.java
+++ my_sandbox/DLList/src/main/java/SLList.java
@@ -1,26 +1,36 @@
 
 public class SLList {
 
-    private IntNode first;
+    private IntNode sentinel;
     private int size;
 
+    public SLList() {
+        this.sentinel = new IntNode(124456, null);
+        this.size = 0;
+    }
+    
     public SLList(int x) {
-        this.first = new IntNode(x, null);
+        this.sentinel = new IntNode(124456, null);
+        this.sentinel.next = new IntNode(x, null);
         this.size = 1;
     }
 
     public void addFirst(int item) {
-        first = new IntNode(item, first);
+        sentinel.next = new IntNode(item, sentinel.next);
         size++;
     }
     
     public void addLast(int item) {
-        IntNode pointer = first;
-        
+        IntNode pointer = sentinel;
+        while(pointer.next != null) {
+            pointer = pointer.next;
+        }
+        size++;
+        pointer.next = new IntNode(item, null);
     }
 
     public int getFirst() {
-        return first.item;
+        return sentinel.next.item;
     }
     
     public int size() {
@@ -31,6 +41,7 @@ public class SLList {
         SLList list = new SLList(15);
         list.addFirst(10);
         list.addFirst(5);
+        list.addLast(25);
         System.out.println(list.getFirst());
     }
 
