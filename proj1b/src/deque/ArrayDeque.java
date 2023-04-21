62e5d3ef399cd158effd96f22839c9e9856c6a55
IceMajor <icemajorr2@gmail.com>
2023-04-21 11:04:52 +0200

    Circular implementation of ArrayDeque: add methods

--- proj1b/src/deque/ArrayDeque.java
+++ proj1b/src/deque/ArrayDeque.java
@@ -1,36 +1,77 @@
 package deque;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class ArrayDeque<T> implements deque.Deque<T> {
 
     public static void main(String[] args) {
         Deque<Integer> list = new ArrayDeque<>();
+        list.addFirst(5);
+        list.addFirst(7);
+        list.addFirst(-2);
+        System.out.println(list.toList());
+    }
+
+    private T[] array;
+    private int backSize;
+    private int frontSize;
+
+    public ArrayDeque() {
+        this.array = (T[]) new Object[8];
+        this.backSize = 0;
+        this.frontSize = 0;
     }
 
     @Override
     public void addFirst(T x) {
-
+        int lastInd = array.length - 1;
+        array[lastInd - backSize] = x;
+        backSize++;
     }
 
     @Override
     public void addLast(T x) {
-
+        array[frontSize] = x;
+        frontSize++;
     }
 
     @Override
     public List<T> toList() {
-        return null;
+        List<T> firstItems = getFirstItems();
+        List<T> lastItems = getLastItems();
+        List<T> list = Stream.concat(firstItems.stream(), lastItems.stream()).toList();
+        return list;
+    }
+
+    private List<T> getFirstItems() {
+        if(backSize == 0) {
+            return new ArrayList<>();
+        }
+        List<T> first = new ArrayList<>();
+        for(int i = array.length - backSize; i < array.length; i++) {
+            first.add(array[i]);
+        }
+        return first;
+    }
+
+    private List<T> getLastItems() {
+        List<T> last = new ArrayList<>();
+        for(int i = 0; i < frontSize; i++) {
+            last.add(array[i]);
+        }
+        return last;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size() == 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.backSize + this.frontSize;
     }
 
     @Override
--- proj1b/tests/ArrayDequeTest.java
+++ proj1b/tests/ArrayDequeTest.java
@@ -1,8 +1,81 @@
+import deque.ArrayDeque;
+import deque.Deque;
+import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
 
 public class ArrayDequeTest {
 
+    @Test
+    public void addFirstBasicTest() {
+        Deque<String> dq = new ArrayDeque<>();
+        dq.addFirst("bek");
+        dq.addFirst("klok");
+        dq.addFirst("bruk");
+
+        assertThat(dq.toList()).containsExactly("bruk", "klok", "bek").inOrder();
+    }
+
+    @Test
+    public void addLastBasicTest() {
+        Deque<Float> dq = new ArrayDeque<>();
+        dq.addLast(3.21f);
+        dq.addLast(-9.09f);
+        dq.addLast(0.00f);
+        dq.addLast(1.00f);
+
+        assertThat(dq.toList()).containsExactly(3.21f, -9.09f, 0.00f, 1.00f).inOrder();
+    }
+
+    @Test
+    public void sizeIncreasesWithAdditionTest() {
+        Deque<Object> dq = new ArrayDeque<>();
+        dq.addLast("5523");
+        dq.addLast(null);
+        dq.addFirst(5.2f);
+        dq.addLast("pekka");
+
+        assertThat(dq.size()).isEqualTo(4);
+    }
+
+    @Test
+    public void addFirstBigDataTest() {
+        Deque<Integer> dq = new ArrayDeque<>();
+        List<Integer> check = new ArrayList<>();
+
+        for(int i = 0; i < 128000; i++) {
+            Random random = new Random();
+            int randomNum = random.nextInt(500000) - 250000;
+            dq.addFirst(randomNum);
+            check.add(randomNum);
+        }
+        List<Integer> reversed = new ArrayList<>();
+        for(int i = check.size() - 1; i >= 0; i--) {
+            reversed.add(check.get(i));
+        }
+        check = reversed;
+        assertThat(dq.toList()).isEqualTo(check);
+    }
+
+    @Test
+    public void addLastBigDataTest() {
+        Deque<Integer> dq = new ArrayDeque<>();
+        List<Integer> check = new ArrayList<>();
+
+        for(int i = 0; i < 128000; i++) {
+            Random random = new Random();
+            int randomNum = random.nextInt(500000) - 250000;
+            dq.addLast(randomNum);
+            check.add(randomNum);
+        }
+
+        assertThat(dq.toList()).isEqualTo(check);
+    }
 }
