b6d5efe2a546ffbf14f71f95e86e5b84eadbd149
IceMajor <icemajorr2@gmail.com>
2023-05-02 11:59:57 +0200

    New take at HW2. With virtual top & bottom

--- hw2/src/Percolation.java
+++ hw2/src/Percolation.java
@@ -10,85 +10,56 @@ public class Percolation {
 
     private WeightedQuickUnionUF wquuf;
     private int[][] grid;
-    private int squareSide;
+    private int side;
     private int openSites;
 
     public Percolation(int N) throws IllegalArgumentException {
         if(N <= 0) {
             throw new IllegalArgumentException();
         }
-        this.squareSide = N;
-        this.grid = new int[squareSide][squareSide];
-        this.wquuf = new WeightedQuickUnionUF(squareSide * squareSide);
+        this.side = N;
+        this.grid = new int[side][side];
         this.openSites = 0;
+
+        this.wquuf = new WeightedQuickUnionUF(side * side + 2);
+        this.createVirtualTop();
+        this.createVirtualBottom();
     }
 
     public void open(int row, int col) throws IndexOutOfBoundsException {
         if(!validIndex(row, col)) {
             throw new IndexOutOfBoundsException();
         }
-
-        this.grid[row][col] = 1;
-        if(row == 0) {
-            fill(row, col);
-        }
-        openSites++;
-
-        unionWithNeighbor(row, col);
     }
 
-    public void fill(int row, int col) throws IndexOutOfBoundsException {
-        if(!validIndex(row, col)) {
-            throw new IndexOutOfBoundsException();
-        }
-        this.grid[row][col] = 2;
-    }
+    private void createVirtualTop() {
+        int[] firstRow = this.grid[0];
 
-    private void unionWithNeighbor(int row, int col) {
-        int thisPos = this.getPosIndex(row, col);
-        int[][] neighbors = getNeighbors(row, col);
-        for(int[] neighbor : neighbors) {
-            if(!isOpen(neighbor[0], neighbor[1])) {
-                continue;
-            }
-            if(isFull(neighbor[0], neighbor[1])) {
-                fill(row, col);
-            }
-            int neighPos = this.getPosIndex(neighbor[0], neighbor[1]);
-            wquuf.union(thisPos, neighPos);
+        int i = 0;
+        for(int square : firstRow) {
+            int squarePos = getPosIndex(0, i);
+            wquuf.union(0, squarePos);
+            i++;
         }
     }
 
-    private int[][] getNeighbors(int row, int col) {
-        int[] left = {row, col - 1};
-        int[] right = {row, col + 1};
-        int[] up = {row - 1, col};
-        int[] down = {row + 1, col};
+    private void createVirtualBottom() {
+        int[] lastRow = this.grid[side - 1];
+        int virtualBottomPos = side * side + 1;
 
-        int[][] temp = {left, right, up, down};
-
-        int[][] temp2 = new int[4][2];
-
-        int count = 0;
-        for(int[] maybe : temp) {
-            if(!validIndex(maybe[0], maybe[1])) {
-                maybe = null;
-                continue;
-            }
-            temp2[count] = maybe;
-            count++;
+        int i = 0;
+        for(int square : lastRow) {
+            int squarePos = getPosIndex(0, i);
+            wquuf.union(virtualBottomPos, squarePos);
+            i++;
         }
-
-        int[][] neighbors = new int[count][2];
-        System.arraycopy(temp2, 0, neighbors, 0, count);
-        return neighbors;
     }
 
     public boolean isOpen(int row, int col) throws IndexOutOfBoundsException {
         if(!validIndex(row, col)) {
             throw new IndexOutOfBoundsException();
         }
-        return this.grid[row][col] == 1 || this.grid[row][col] == 2;
+        return isFull(row, col) || this.grid[row][col] == 1;
     }
 
     public boolean isFull(int row, int col) throws IndexOutOfBoundsException {
@@ -103,29 +74,9 @@ public class Percolation {
     }
 
     public boolean percolates() {
-        int[] lastRow = this.grid[squareSide - 1];
-        int[] firstRow = this.grid[0];
-
-        int lastColPointer = -1;
-        int firstColPointer = -1;
-        for(int square : lastRow) {
-            lastColPointer++;
-            int squarePos = this.getPosIndex(squareSide - 1, lastColPointer);
-            if(!isFull(squareSide - 1, lastColPointer)) {
-                continue;
-            }
-            for(int _square : firstRow) {
-                firstColPointer++;
-                int _squarePos = this.getPosIndex(0, firstColPointer);
-                if(!isFull(0, firstColPointer)) {
-                    continue;
-                }
-                if(wquuf.connected(squarePos, _squarePos)) {
-                    return true;
-                }
-            }
-        }
-        return false;
+        int virtualBottomPos = side * side + 1;
+        int virtualTopPos = 0;
+        return wquuf.connected(virtualTopPos, virtualBottomPos);
     }
 
     private boolean validIndex(int row, int col) {
@@ -139,7 +90,7 @@ public class Percolation {
         if(!validIndex(row, col)) {
             throw new IndexOutOfBoundsException();
         }
-        return row * squareSide + col;
+        return row * side + col + 1;
     }
 
     // TODO: Add any useful helper methods (we highly recommend this!).
