787a7ca1bdd7b0ce0040a1bd422bfa01bda8ec47
IceMajor <icemajorr2@gmail.com>
2023-05-06 16:03:47 +0200

    Adding TimeSeries

--- proj2a/ngordnet/ngrams/TimeSeries.java
+++ proj2a/ngordnet/ngrams/TimeSeries.java
@@ -31,8 +31,8 @@ public class TimeSeries extends TreeMap<Integer, Double> {
     public TimeSeries(TimeSeries ts, int startYear, int endYear) {
         this();
 
-        for(Integer year : ts.years()) {
-            if(!(year >= startYear && year <= endYear)) {
+        for (int year : ts.years()) {
+            if (!(year >= startYear && year <= endYear)) {
                 continue;
             }
             double data = ts.get(year);
@@ -61,21 +61,33 @@ public class TimeSeries extends TreeMap<Integer, Double> {
      * Returns the year-wise sum of this TimeSeries with the given TS. In other words, for
      * each year, sum the data from this TimeSeries with the data from TS. Should return a
      * new TimeSeries (does not modify this TimeSeries).
-     *
+     * <p>
      * If both TimeSeries don't contain any years, return an empty TimeSeries.
      * If one TimeSeries contains a year that the other one doesn't, the returned TimeSeries
      * should store the value from the TimeSeries that contains that year.
      */
     public TimeSeries plus(TimeSeries ts) {
-        // TODO: Fill in this method.
-        return null;
+        List<Integer> yearsThis = this.years();
+
+        List<Integer> yearsOther = ts.years();
+        List<Double> dataOther = ts.data();
+
+        TimeSeries newTs = new TimeSeries(this, yearsThis.get(0), yearsThis.get(yearsThis.size() - 1));
+        for (int i = 0; i < yearsOther.size(); i++) {
+            int year = yearsOther.get(i);
+            double data = dataOther.get(i);
+
+            newTs.put(year, newTs.getOrDefault(year, 0d) + data);
+        }
+
+        return newTs;
     }
 
     /**
      * Returns the quotient of the value for each year this TimeSeries divided by the
      * value for the same year in TS. Should return a new TimeSeries (does not modify this
      * TimeSeries).
-     *
+     * <p>
      * If TS is missing a year that exists in this TimeSeries, throw an
      * IllegalArgumentException.
      * If TS has a year that is not in this TimeSeries, ignore it.
