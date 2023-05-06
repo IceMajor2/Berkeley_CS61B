5a08a9d509a780b322c6a89e89e65e94b199fff2
IceMajor <icemajorr2@gmail.com>
2023-05-06 18:54:38 +0200

    Chart handler

---
+++ proj2a/ngordnet/main/HistoryHandler.java
@@ -0,0 +1,36 @@
+package ngordnet.main;
+
+import ngordnet.browser.NgordnetQuery;
+import ngordnet.browser.NgordnetQueryHandler;
+import ngordnet.ngrams.NGramMap;
+import ngordnet.ngrams.TimeSeries;
+import ngordnet.plotting.Plotter;
+import org.knowm.xchart.XYChart;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HistoryHandler extends NgordnetQueryHandler {
+
+    private NGramMap ngm;
+
+    public HistoryHandler(NGramMap map) {
+        this.ngm = map;
+    }
+
+    @Override
+    public String handle(NgordnetQuery q) {
+        List<String> words = q.words();
+        int startYear = q.startYear();
+        int endYear = q.endYear();
+
+        List<TimeSeries> lts = new ArrayList<>();
+        for(String word : words) {
+            lts.add(ngm.weightHistory(word, startYear, endYear));
+        }
+
+        XYChart chart = Plotter.generateTimeSeriesChart(words, lts);
+        String encodedImage = Plotter.encodeChartAsString(chart);
+        return encodedImage;
+    }
+}
